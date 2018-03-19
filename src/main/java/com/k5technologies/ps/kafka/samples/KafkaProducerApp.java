2bb3766043de08ab046939270aa4ae89d5dea430
Joey K <jkrabacher@gmail.com>
2018-03-18 21:54:45 -0500

    -- formatting

--- src/main/java/com/k5technologies/ps/kafka/samples/KafkaProducerApp.java
+++ src/main/java/com/k5technologies/ps/kafka/samples/KafkaProducerApp.java
@@ -7,30 +7,30 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 
 /**
  * Kafka Sample
+ * 
  * @author Joey Krabacher ( K5 Technologies)
  *
  */
-public class KafkaProducerApp 
-{
-    public static void main( String[] args )
-    {
-        Properties props = new Properties();
-        props.put("bootstrap.servers", "localhost:9092, localhost:9093, localhost:9094");
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        
-        KafkaProducer<String, String> myProducer = new KafkaProducer<String, String>(props);
-        
-        try {
-        	// send over 25 messages
-        	for (int i = 0; i < 25; i++) {
-        		myProducer.send(new ProducerRecord<String, String>("my_topic", Integer.toString(i), "My Message " + Integer.toString(i)));
+public class KafkaProducerApp {
+	public static void main(String[] args) {
+		Properties props = new Properties();
+		props.put("bootstrap.servers", "localhost:9092, localhost:9093, localhost:9094");
+		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+		KafkaProducer<String, String> myProducer = new KafkaProducer<String, String>(props);
+
+		try {
+			// send over 25 messages
+			for (int i = 0; i < 25; i++) {
+				myProducer.send(new ProducerRecord<String, String>("my_topic", Integer.toString(i),
+						"My Message " + Integer.toString(i)));
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}finally {
+		} finally {
 			myProducer.close();
 		}
-    }
+	}
 }
